3e1d17b87b7815af0725099de7942c62ab18a7af
lenaabdu <lenaabdu23@gmail.com>
2021-10-26 14:57:04 +0100

    Updates

---
+++ precache-manifest.3196755321990d18ff564acefd4ebcb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a6251dcbac5bbadb7c5684f6a89d8f",
+    "url": "/front-end-Test/index.html"
+  },
+  {
+    "revision": "9015f9079a6f90f2240f",
+    "url": "/front-end-Test/static/css/2.aacd8a78.chunk.css"
+  },
+  {
+    "revision": "dcab0595dce0bf4bfb2a",
+    "url": "/front-end-Test/static/css/main.c335a0b2.chunk.css"
+  },
+  {
+    "revision": "9015f9079a6f90f2240f",
+    "url": "/front-end-Test/static/js/2.67abf969.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/front-end-Test/static/js/2.67abf969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcab0595dce0bf4bfb2a",
+    "url": "/front-end-Test/static/js/main.925e1bbb.chunk.js"
+  },
+  {
+    "revision": "7218cf6876635b9a0246",
+    "url": "/front-end-Test/static/js/runtime-main.1e679482.js"
+  }
+]);
